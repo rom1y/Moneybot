ef2905a72126eac61a68bd5a5e604605e252ba49
rom1y <125592846+rom1y@users.noreply.github.com>
2023-05-09 15:41:25 +0500

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-BOT_API = "6190982626:AAFyk9swd3PsnBDoKDYOkMT2zGK_WsqC8eU"
-
-YOOKASSA_API = "381764678:TEST:56381"
-
-CPA_CHAT = "https://t.me/costperactionl"
-
-CPA_CHANNEL = "https://t.me/costperuaction"
